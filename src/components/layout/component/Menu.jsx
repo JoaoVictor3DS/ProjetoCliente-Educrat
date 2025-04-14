141d4d54cc6579bfd676a4c10868b84b55921332
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-04-14 07:37:31 -0300

    Fix page404 go to home button

--- src/components/layout/component/Menu.jsx
+++ src/components/layout/component/Menu.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import MobileFooter from "./MobileFooter";
 
-import { menuList } from "@/data/menu";
 import { useLocation } from "react-router-dom";
 
 export default function Menu({ allClasses, headerPosition }) {
