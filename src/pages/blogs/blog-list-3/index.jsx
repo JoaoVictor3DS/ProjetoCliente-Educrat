af767cef2390c0e38d9af8a8979c87c7c8711825
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-05-05 10:44:00 -0300

    Change blog title

--- src/pages/blogs/blog-list-3/index.jsx
+++ src/pages/blogs/blog-list-3/index.jsx
@@ -9,9 +9,9 @@ import React from "react";
 import MetaComponent from "@/components/common/MetaComponent";
 
 const metadata = {
-	title: "Blog-list-3 || Educrat - Professional LMS Online Education Course ReactJS Template",
+	title: "Meu Curso, Minha Vida",
 	description:
-		"Elevate your e-learning content with Educrat, the most impressive LMS template for online courses, education and LMS platforms.",
+		"Eleve seu conteúdo de e-learning com o Meu Curso, Minha Vida.",
 };
 
 export default function BlogListpage3() {
